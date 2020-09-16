0a4f974a103ce94182f99c717f6457c7f84c085b
jeremyqzt <jq357@hotmail.com>
2020-09-16 18:22:43 -0400

    Expanded contact page

---
+++ lib/screens/V2/expanded_contact.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
+
+class ExpandedContact extends StatefulWidget {
+  ExpandedContact();
+
+  @override
+  State<StatefulWidget> createState() {
+    return _ExpandedContact();
+  }
+}
+
+class _ExpandedContact extends State<ExpandedContact> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
