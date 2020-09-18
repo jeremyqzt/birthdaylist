e057b586344dfdd9c378292c3a61a549657ad715
jeremyqzt <jq357@hotmail.com>
2020-09-17 22:25:09 -0400

    Removed tabs

--- lib/screens/V2/expanded_contact.dart
+++ lib/screens/V2/expanded_contact.dart
@@ -14,13 +14,11 @@ class _ExpandedContact extends State<ExpandedContact> {
     @override
     Widget build(BuildContext context) {
       return Scaffold(
-          body: DefaultTabController(
-            length: 2,
-            child: NestedScrollView(
+          body: NestedScrollView(
               headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
                 return [
                   SliverAppBar(
-                    expandedHeight: 200.0,
+                    expandedHeight: 150.0,
                     floating: true,
                     pinned: true,
                     snap: true,
@@ -35,29 +33,12 @@ class _ExpandedContact extends State<ExpandedContact> {
                       ],
                     ),
                   ),
-                  SliverPadding(
-                    padding: new EdgeInsets.all(16.0),
-                    sliver: new SliverList(
-                      delegate: new SliverChildListDelegate([
-                        TabBar(
-                          labelColor: Colors.black87,
-                          unselectedLabelColor: Colors.grey,
-                          tabs: [
-                            new Tab(icon: new Icon(Icons.info), text: "Tab 1"),
-                            new Tab(
-                                icon: new Icon(Icons.lightbulb_outline),
-                                text: "Tab 2"),
-                          ],
-                        ),
-                      ]),
-                    ),
-                  ),
                 ];
               },
               body: Center(
                 child: Text("Sample text"),
               ),
             ),
-          ));
+          );
     }
 }
