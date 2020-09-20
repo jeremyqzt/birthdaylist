dfe56cc0209afcc19849d703f5ddc94d7b914730
jeremyqzt <jq357@hotmail.com>
2020-09-20 15:47:54 -0400

    removed sample code...

--- lib/screens/V2/expanded_contact.dart
+++ lib/screens/V2/expanded_contact.dart
@@ -17,22 +17,7 @@ class _ExpandedContact extends State<ExpandedContact> {
           body: NestedScrollView(
               headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
                 return [
-                  SliverAppBar(
-                    expandedHeight: 150.0,
-                    floating: true,
-                    pinned: true,
-                    snap: true,
-                    actionsIconTheme: IconThemeData(opacity: 0.0),
-                    flexibleSpace: Stack(
-                      children: <Widget>[
-                        Positioned.fill(
-                            child: Image.asset(
-                              "assets/images/stars.jpg",
-                              fit: BoxFit.cover,
-                            ))
-                      ],
-                    ),
-                  ),
+                  getAppBar("♏ Jeremy Qian"),
                 ];
               },
               body: Center(
@@ -41,99 +26,29 @@ class _ExpandedContact extends State<ExpandedContact> {
             ),
           );
     }
-}
 
-
-class MySample extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return SafeArea(
-      child: Material(
-        child: CustomScrollView(
-          slivers: [
-            SliverPersistentHeader(
-              delegate: MySliverAppBar(expandedHeight: 200),
+    Widget getAppBar(String headerText) {
+        return
+            SliverAppBar(
+              expandedHeight: 200.0,
+              floating: false,
               pinned: true,
-            ),
-            SliverList(
-              delegate: SliverChildBuilderDelegate(
-                    (_, index) => ListTile(
-                  title: Text("Index: $index"),
+              leading: Icon(Icons.arrow_back),
+              flexibleSpace: FlexibleSpaceBar(
+                centerTitle: true,
+                title: Text(
+                  headerText,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16.0,
+                  ),
+                ),
+                background: Image.asset(
+                  "assets/images/stars.jpg",
+                  fit: BoxFit.cover,
                 ),
               ),
-            )
-          ],
-        ),
-      ),
-    );
-  }
+            );
+    }
 }
 
-class MySliverAppBar extends SliverPersistentHeaderDelegate {
-  final double expandedHeight;
-
-  MySliverAppBar({@required this.expandedHeight});
-
-  @override
-  Widget build(
-      BuildContext context, double shrinkOffset, bool overlapsContent) {
-    return Stack(
-      fit: StackFit.expand,
-      overflow: Overflow.visible,
-      children: [
-        Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: [
-                Color(0XFF2E5596),
-                Color(0XFF16304E),
-              ],
-            ),
-          ),
-        ),
-        Center(
-          child: Opacity(
-            opacity: shrinkOffset / expandedHeight,
-            child: Text(
-              "MySliverAppBar",
-              style: TextStyle(
-                color: Colors.white,
-                fontWeight: FontWeight.w700,
-                fontSize: 23,
-              ),
-            ),
-          ),
-        ),
-        Positioned(
-          top: expandedHeight / 2 - shrinkOffset,
-          left: MediaQuery.of(context).size.width / 3.5,
-          child: Opacity(
-            opacity: (1 - shrinkOffset / expandedHeight),
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
-                shape: BoxShape.circle,
-                border: Border.all(color: Colors.yellow, width: 3),
-              ),
-              child: SizedBox(
-                height: expandedHeight,
-                width: MediaQuery.of(context).size.width / 2.5,
-              ),
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-
-  @override
-  double get maxExtent => expandedHeight;
-
-  @override
-  double get minExtent => kToolbarHeight;
-
-  @override
-  bool shouldRebuild(SliverPersistentHeaderDelegate oldDelegate) => true;
-}
