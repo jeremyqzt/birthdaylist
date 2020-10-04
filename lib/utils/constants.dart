8e2b2a93d54cd6caaa32efa31eaa4653958e69dc
jeremyqzt <jq357@hotmail.com>
2020-10-04 09:44:06 -0400

    EventMap

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -13,3 +13,13 @@ enum DateTypes {
   SOLAR,
   INVALID
 }
+
+Map<SpecialDays, String> EventMap = {
+  SpecialDays.BIRTHDAY: "Birthday",
+  SpecialDays.ANNIVERSARY: "Anniversary",
+  SpecialDays.FIRSTOF: "first",
+  SpecialDays.ROUTINE: "repeating",
+  SpecialDays.OTHER: "",
+  SpecialDays.NOTHING: "null-ish (Opps, we have an error here)",
+  SpecialDays.INVALID: "??? (Opps, we have an error here)",
+};
